66b90638f6fa4c5dcf42a9538a6d0b89613b8ebd
jeancdev <jcode2006@gmail.com>
2026-04-29 22:38:08 -0500

    feat(dynamodb): add jobs table configuration with attributes, outputs, and variables

---
+++ iac/modules/data_layer/dynamodb/variables.tf
@@ -0,0 +1,36 @@
+variable "name_prefix" {
+  description = "Prefix used to name DynamoDB resources"
+  type        = string
+}
+
+variable "jobs_table_name" {
+  description = "DynamoDB jobs table name"
+  type        = string
+}
+
+variable "jobs_table_hash_key" {
+  description = "DynamoDB partition key attribute name"
+  type        = string
+}
+
+variable "jobs_table_range_key" {
+  description = "DynamoDB sort key attribute name"
+  type        = string
+}
+
+variable "enable_point_in_time_recovery" {
+  description = "Enable PITR on jobs table"
+  type        = bool
+}
+
+variable "enable_ttl" {
+  description = "Enable TTL on jobs table for auto-cleanup of old records"
+  type        = bool
+  default     = true
+}
+
+variable "enable_gsi_client_status" {
+  description = "Enable GSI clientId-status-index for querying jobs by scientist"
+  type        = bool
+  default     = true
+}
