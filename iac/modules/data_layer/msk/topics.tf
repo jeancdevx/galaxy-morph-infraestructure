a566ecb7a1c438de7b43b12e34584b20a3f37bae
jeancdev <jcode2006@gmail.com>
2026-05-13 20:33:46 -0500

    fix(data-layer): correct kafka topic partitions to 24 and replication factor to 2

--- iac/modules/data_layer/msk/topics.tf
+++ iac/modules/data_layer/msk/topics.tf
@@ -1,18 +1,21 @@
+# Topic creation is currently done outside Terraform from CloudShell/CI runners
+# that have network path to MSK in VPC and IAM auth for Kafka operations.
+#
 # resource "kafka_topic" "ingestion" {
-#  name               = "galaxy.ingestion"
-#   replication_factor = 3
-#   partitions         = 50
-# 
+#   name               = "galaxy.ingestion"
+#   replication_factor = 2
+#   partitions         = 24
+#
 #   config = {
 #     "retention.ms" = "604800000"
 #   }
 # }
-# 
+#
 # resource "kafka_topic" "results" {
 #   name               = "galaxy.results"
-#   replication_factor = 3
-#   partitions         = 50
-# 
+#   replication_factor = 2
+#   partitions         = 24
+#
 #   config = {
 #     "retention.ms" = "259200000"
 #   }
