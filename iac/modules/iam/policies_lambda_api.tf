b956005036f39cbfdf311ddbbe0e5d3066b09f70
jeancdev <jcode2006@gmail.com>
2026-05-20 17:00:27 -0500

    feat(iam): add lambda-api role and post-confirmation role with scoped policies

---
+++ iac/modules/iam/policies_lambda_api.tf
@@ -0,0 +1,53 @@
+data "aws_iam_policy_document" "lambda_api" {
+  statement {
+    sid = "CloudWatchLogs"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "CognitoAuth"
+
+    actions = [
+      "cognito-idp:InitiateAuth",
+      "cognito-idp:SignUp",
+      "cognito-idp:ConfirmSignUp",
+      "cognito-idp:ResendConfirmationCode",
+    ]
+
+    resources = [
+      var.cognito_user_pool_id != "" ?
+      "arn:aws:cognito-idp:${var.aws_region}:${var.aws_account_id}:userpool/${var.cognito_user_pool_id}" :
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "DynamoDBJobsQuery"
+
+    actions = [
+      "dynamodb:Query",
+    ]
+
+    resources = [
+      local.jobs_table_arn,
+      "${local.jobs_table_arn}/index/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_api" {
+  name   = "${var.name_prefix}-lambda-api-policy"
+  policy = data.aws_iam_policy_document.lambda_api.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_api" {
+  role       = aws_iam_role.lambda_api.name
+  policy_arn = aws_iam_policy.lambda_api.arn
+}
