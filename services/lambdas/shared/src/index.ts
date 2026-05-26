210f422e200655b897f3acee55c862568ddbd6ea
jeancdev <jcode2006@gmail.com>
2026-05-26 18:46:41 -0500

    feat(lambdas): add @galaxy-morph/shared package with domain types and HTTP helpers

---
+++ services/lambdas/shared/src/index.ts
@@ -0,0 +1,9 @@
+export { jsonResponse } from './http.js'
+export { POWERTOOLS_NAMESPACE } from './types.js'
+export type {
+  ClassificationResult,
+  ClassificationStatus,
+  ImageRef,
+  JobRecord,
+  KafkaResultMessage
+} from './types.js'
