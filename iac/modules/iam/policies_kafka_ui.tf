ca05f288d555e93d3a3b971cd49d4c9960969454
jeancdev <jcode2006@gmail.com>
2026-05-26 22:27:31 -0500

    refactor(iam): rewrite kafka-ui MSK policy using aws_iam_policy_document scoped by resource type

--- iac/modules/iam/policies_kafka_ui.tf
+++ iac/modules/iam/policies_kafka_ui.tf
@@ -19,37 +19,55 @@ resource "aws_iam_role" "kafka_ui_task" {
   }
 }
 
+data "aws_iam_policy_document" "kafka_ui_msk_access" {
+  statement {
+    sid = "MSKClusterConnect"
+
+    actions = [
+      "kafka-cluster:Connect",
+      "kafka-cluster:DescribeCluster",
+      "kafka-cluster:DescribeClusterDynamicConfiguration",
+      "kafka-cluster:AlterCluster",
+      "kafka-cluster:AlterClusterDynamicConfiguration",
+    ]
+
+    resources = [var.msk_cluster_arn]
+  }
+
+  statement {
+    sid = "MSKTopicManage"
+
+    actions = [
+      "kafka-cluster:DescribeTopic",
+      "kafka-cluster:DescribeTopicDynamicConfiguration",
+      "kafka-cluster:CreateTopic",
+      "kafka-cluster:AlterTopic",
+      "kafka-cluster:AlterTopicDynamicConfiguration",
+      "kafka-cluster:DeleteTopic",
+      "kafka-cluster:ReadData",
+      "kafka-cluster:WriteData",
+    ]
+
+    resources = [var.msk_topic_arn_prefix]
+  }
+
+  statement {
+    sid = "MSKGroupManage"
+
+    actions = [
+      "kafka-cluster:DescribeGroup",
+      "kafka-cluster:AlterGroup",
+      "kafka-cluster:DeleteGroup",
+    ]
+
+    resources = [var.msk_group_arn_prefix]
+  }
+}
+
 resource "aws_iam_policy" "kafka_ui_msk_access" {
   name        = "${var.name_prefix}-kafka-ui-msk-access"
   description = "Allow Kafka UI to connect and read/write to MSK Serverless"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "kafka-cluster:Connect",
-          "kafka-cluster:DescribeCluster",
-          "kafka-cluster:DescribeClusterDynamicConfiguration",
-          "kafka-cluster:AlterCluster",
-          "kafka-cluster:AlterClusterDynamicConfiguration",
-          "kafka-cluster:DescribeTopic",
-          "kafka-cluster:DescribeTopicDynamicConfiguration",
-          "kafka-cluster:CreateTopic",
-          "kafka-cluster:AlterTopic",
-          "kafka-cluster:AlterTopicDynamicConfiguration",
-          "kafka-cluster:DeleteTopic",
-          "kafka-cluster:ReadData",
-          "kafka-cluster:WriteData",
-          "kafka-cluster:DescribeGroup",
-          "kafka-cluster:AlterGroup",
-          "kafka-cluster:DeleteGroup"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
+  policy      = data.aws_iam_policy_document.kafka_ui_msk_access.json
 }
 
 resource "aws_iam_role_policy_attachment" "kafka_ui_msk_access" {
